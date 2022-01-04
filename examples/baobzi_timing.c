d040d631a0d3fa2cc173abf2e32fc9574d4d0e1d
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-01-04 08:53:05 -0500

    Add tolerance parameter for C timing example

--- examples/baobzi_timing.c
+++ examples/baobzi_timing.c
@@ -57,14 +57,14 @@ void print_error(const baobzi_t *function, const double *x, int size) {
 }
 
 void test_func(double (*fin)(const double *), int dim, int order, const double *xin, const double *hl,
-               const double *center, int n_points, int n_runs) {
+               const double *center, int n_points, int n_runs, double tol) {
     double *x_transformed = (double *)aligned_alloc(32, n_points * dim * sizeof(double));
 
     for (int i = 0; i < dim * n_points; i += dim)
         for (int j = 0; j < dim; ++j)
             x_transformed[i + j] = hl[j] * (2.0 * xin[i + j] - 1.0) + center[j];
 
-    baobzi_t func_approx = baobzi_init(&testfun_2d, dim, order, center, hl, 1E-8);
+    baobzi_t func_approx = baobzi_init(&testfun_2d, dim, order, center, hl, tol);
 
     time_function(&func_approx, x_transformed, n_points * dim, n_runs);
     print_error(&func_approx, x_transformed, n_points * dim);
@@ -81,8 +81,8 @@ int main(int argc, char *argv[]) {
     if (argc == 2)
         n_runs = atoi(argv[1]);
 
-    double *x = (double *)aligned_alloc(32, n_points * 3 * sizeof(double));
-    for (size_t i = 0; i < n_points * 3; ++i)
+    double *x = (double *)aligned_alloc(32, n_points * 5 * sizeof(double));
+    for (size_t i = 0; i < n_points * 5; ++i)
         x[i] = ((double)rand()) / RAND_MAX;
 
     {
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
         const int order = 6;
         double hl[2] = {1.0, 1.0};
         double center[2] = {hl[0] + 0.5, hl[1] + 2.0};
-        test_func(&testfun_2d, dim, order, x, hl, center, n_points, n_runs);
+        test_func(&testfun_2d, dim, order, x, hl, center, n_points, n_runs, 1E-10);
     }
 
     {
@@ -98,7 +98,7 @@ int main(int argc, char *argv[]) {
         const int order = 6;
         double hl[3] = {1.0, 1.0, 1.0};
         double center[3] = {hl[0] + 0.5, hl[1] + 2.0, hl[2] + 0.5};
-        test_func(&testfun_3d, dim, order, x, hl, center, n_points, n_runs);
+        test_func(&testfun_3d, dim, order, x, hl, center, n_points, n_runs, 1E-10);
     }
 
     return 0;
