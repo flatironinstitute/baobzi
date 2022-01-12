52660c04aedb7d3fe7a6cdbb63bf0f77a29d2f1d
Robert Blackwell <rblackwell@scclin016.flatironinstitute.org>
2022-01-12 08:55:12 -0500

    Add save/restore to julia bindings #6

--- julia/examples/simple2d.jl
+++ julia/examples/simple2d.jl
@@ -12,6 +12,13 @@ test_point = [0.25, 0.25]
 dim = 2
 order = 6
 tol = 1E-8
+output_file = "simple2d.baobzi"
 
 func_approx = baobzi.init(testfunc, dim, order, center, hl, tol)
+println(baobzi.eval(func_approx, test_point) - testfunc(pointer(test_point)))
+
+baobzi.save(func_approx, output_file)
+baobzi.free(func_approx)
+
+func_approx = baobzi.restore(testfunc, output_file)
 println(baobzi.eval(func_approx, test_point) - testfunc(pointer(test_point)))
