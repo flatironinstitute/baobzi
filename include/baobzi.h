df5a512fe93c67d3878903cc299133eee4d5ff7e
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-01-21 12:28:36 -0500

    Add more developer documentation #23

--- include/baobzi.h
+++ include/baobzi.h
@@ -10,24 +10,58 @@
 extern "C" {
 #endif
 
+/// @brief Baobzi C structure for a common API through C bindings. All work is done through the
+/// pointer baobzi_t though.
+///
+/// Contains pointers to the wrappers of the relevant template C++ functions for a
+/// dim+order+instruction set
 typedef struct {
-    void *obj;
-    int DIM;
-    int ORDER;
-    double (*f_)(const double *);
-    double (*eval)(const void *, const double *);
-    void (*save)(const void *, const char *);
-    void (*free)(void *);
+    void *obj; ///< Actual baobzi::Function object
+    int DIM; ///< Dimension of our function
+    int ORDER; ///< Order of the polynomial
+    double (*f_)(const double *); ///< Function our approximator represents
+    double (*eval)(const void *, const double *); ///< Pointer to evaluation function
+    void (*save)(const void *, const char *); ///< Pointer to save function
+    void (*free)(void *); ///< pointer to save function
 } baobzi_struct;
 
+/// Our type for the C API
 typedef baobzi_struct* baobzi_t;
 
+/// @brief eval approximator at point x
+/// @param[in] func initialized C baobzi object
+/// @param[in] x point to evaluate at
+/// @returns approximation of function at x
 double baobzi_eval(const baobzi_t func, const double *x);
+
+/// @brief save approximator to file
+/// @param[in] func initialized C baobzi object
+/// @param[in] filename path to output file
 void baobzi_save(const baobzi_t func, const char *filename);
+
+/// @brief restore approximator from file
+/// @param[in] filename path to serialized baobzi file
+/// @returns initialized baobzi C object
 baobzi_t baobzi_restore(const char *filename);
+
+/// @brief free all memory associated with C baobzi object
+/// @returns nullptr
 baobzi_t baobzi_free(baobzi_t func);
+
+/// @brief Construct C baobzi object from input function
+/// @param[in] fin pointer to function to fit
+/// @param[in] dim input dimension of the function
+/// @param[in] order order of polynomial to fit
+/// @param[in] center [dim] center of the domain
+/// @param[in] half_length [dim] half the size of the domain in each dimension
+/// @param[in] tol desired relative tolerance
+/// @returns initialized baobzi C object
 baobzi_t baobzi_init(double (*)(const double *), uint16_t dim, uint16_t order, const double *center,
                      const double *half_length, const double tol);
+
+/// @brief read dim/order/version info from file
+/// @param[in] fname path to input file
+/// @returns file header
 baobzi_header_t baobzi_read_header_from_file(const char *fname);
 
 #include "baobzi/baobzi_decls.h"
