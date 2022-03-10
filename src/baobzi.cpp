42d509a2c8533a2eb74af80e940330321de4b524
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-03-10 15:03:09 -0500

    Add some more work toward windows compatibility
    Remove mmap/stat stuff and just use generic ifstream. Less memory efficient, but way more
    portable.

--- src/baobzi.cpp
+++ src/baobzi.cpp
@@ -3,50 +3,22 @@
 #include "baobzi.h"
 #include "baobzi.hpp"
 
-#include <exception>
-#include <stdexcept>
 #include <tuple>
-#include <unistd.h>
 
 #include <cstdint>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-
 const baobzi_input_t baobzi_input_default = {
     .func = NULL, .data = NULL, .dim = 0, .order = 0, .tol = 0.0, .minimum_leaf_fraction = 0.0, .split_multi_eval = 1};
 
-/// @brief mmap a file
-struct mmap_wrapper {
-    char *addr;         ///< Underlying buffer
-    int fd;             ///< Underlying file descriptor
-    std::size_t buflen; ///< Size of file/buffer
-
-    /// @brief Construct mmap_wrapper object
-    /// @param[in] fname path to file to map
-    mmap_wrapper(const std::string &fname) {
-        fd = open(fname.c_str(), O_RDONLY);
-        if (fd == -1)
-            throw std::runtime_error("Unable to open baobzi file " + fname + ".");
-
-        struct stat sb;
-        if (fstat(fd, &sb) == -1)
-            throw std::runtime_error("Error statting " + fname + ".");
-
-        buflen = sb.st_size;
-
-        addr = static_cast<char *>(mmap(NULL, buflen, PROT_READ, MAP_PRIVATE, fd, 0u));
-        if (addr == MAP_FAILED)
-            throw std::runtime_error("Error mapping " + fname + " for restore.");
-    }
-
-    /// @brief close file descriptor on destruction
-    ~mmap_wrapper() { close(fd); }
-};
+inline std::string file_to_string(const std::string &path) {
+    std::ostringstream buf;
+    std::ifstream input(path.c_str());
+    buf << input.rdbuf();
+    return buf.str();
+}
 
 extern "C" {
 
@@ -92,17 +64,10 @@ void baobzi_eval_multi(const baobzi_t func, const double *x, double *res, int nt
 
 void baobzi_save(const baobzi_t func, const char *filename) { func->save(func->obj, filename); }
 
-baobzi_header_t read_header(const char *addr, std::size_t buflen, std::size_t *offset) {
+baobzi_header_t read_header(const char *addr, const std::size_t buflen, std::size_t *offset) {
     msgpack::object_handle oh;
-    msgpack::unpack(oh, addr, buflen, *offset);
+    msgpack::unpack(oh, addr, buflen, *offset); // actually increments offset
     return oh.get().as<baobzi_header_t>();
-}
-
-baobzi_header_t baobzi_read_header_from_file(const char *fname) {
-    std::size_t offset = 0;
-    std::string filename(fname);
-    mmap_wrapper infile(filename);
-    return read_header(infile.addr, infile.buflen, &offset);
 }
 
 baobzi_t baobzi_restore(const char *filename_cstr) {
@@ -110,12 +75,11 @@ baobzi_t baobzi_restore(const char *filename_cstr) {
     baobzi_t res = (baobzi_t)malloc(sizeof(baobzi_struct));
 
     std::size_t offset = 0;
-    mmap_wrapper infile(filename);
-
-    baobzi_header_t header = read_header(infile.addr, infile.buflen, &offset);
+    std::string filedata_str = file_to_string(filename);
+    baobzi_header_t header = read_header(filedata_str.data(), filedata_str.size(), &offset);
 
     msgpack::object_handle oh;
-    msgpack::unpack(oh, infile.addr, infile.buflen, offset);
+    msgpack::unpack(oh, filedata_str.data(), filedata_str.size(), offset);
     msgpack::object obj = oh.get();
 
     res->DIM = header.dim;
