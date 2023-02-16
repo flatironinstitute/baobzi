8e5bd2d07ef2c8dff1050d4e8885aa3edd33995a
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-02-16 07:26:06 -0500

    python: Add long description to setup for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from skbuild import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='baobzi',
       version='0.9.4',
       description='An adaptive fast function approximator based on tree search',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Robert Blackwell',
       author_email='rblackwell@flatironinstitute.org',
       url='https://github.com/flatironinstitute/baobzi',
